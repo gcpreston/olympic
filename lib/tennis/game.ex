410e3b06bb2385210edfa98293fb187c5e50950b
Graham Preston <graham.preston@appfolio.com>
2024-08-07 17:39:28 -0700

    Draft 1 of working tennis game states

--- lib/tennis/game.ex
+++ lib/tennis/game.ex
@@ -3,6 +3,22 @@ defmodule Tennis.Game do
   Model for a single game of tennis.
   """
 
+  defmodule Deuce do
+    @type state() :: :"40-40" | :"ad-40" | :"40-ad"
+    @type player() :: :p1 | :p2
+
+    @spec new() :: :"40-40"
+    def new, do: :"40-40"
+
+    @spec next_state(state(), player()) :: state() | {:win, player()}
+    def next_state(:"40-40", :p1), do: :"ad-40"
+    def next_state(:"40-40", :p2), do: :"40-ad"
+    def next_state(:"ad-40", :p1), do: {:win, :p1}
+    def next_state(:"ad-40", :p2), do: :"40-40"
+    def next_state(:"40-ad", :p1), do: :"40-40"
+    def next_state(:"40-ad", :p2), do: {:win, :p2}
+  end
+
   @type score() :: 0 | 15 | 30 | 40
   @type regular_state() :: {:regular, {score(), score()}} # TODO: except 40-40
   @type deuce_state() :: {:deuce, Deuce.state()}
@@ -12,36 +28,26 @@ defmodule Tennis.Game do
   @spec new() :: {:regular, {0, 0}}
   def new, do: {:regular, {0, 0}}
 
-  @spec next_state(state(), player()) :: {:cont, state()} | {:win, player()}
+  @spec next_state(state(), player()) :: state() | {:win, player()}
 
-  def next_state({:deuce, deuce_state}, player), do: Deuce.next_state(deuce_state, player)
+  # TODO: I don't totally like this...wish I could send through directly
+  def next_state({:deuce, deuce_state}, player) do
+    case Deuce.next_state(deuce_state, player) do
+      {:win, p} -> {:win, p}
+      new_deuce_state -> {:deuce, new_deuce_state}
+    end
+  end
+
   def next_state({:regular, {30, 40}}, :p1), do: {:deuce, Deuce.new()}
-  def next_state(:regular, {40, 30}, :p2), do: {:deuce, Deuce.new()}
+  def next_state({:regular, {40, 30}}, :p2), do: {:deuce, Deuce.new()}
 
   def next_state({:regular, {0, p2}}, :p1), do: {:regular, {15, p2}}
   def next_state({:regular, {15, p2}}, :p1), do: {:regular, {30, p2}}
-  def next_state({:regular, {30, p2}}, :p1), do: {:regular, 40, p2}
+  def next_state({:regular, {30, p2}}, :p1), do: {:regular, {40, p2}}
   def next_state({:regular, {40, _p2}}, :p1), do: {:win, :p1}
 
   def next_state({:regular, {p1, 0}}, :p2), do: {:regular, {p1, 15}}
   def next_state({:regular, {p1, 15}}, :p2), do: {:regular, {p1, 30}}
   def next_state({:regular, {p1, 30}}, :p2), do: {:regular, {p1, 40}}
   def next_state({:regular, {_p1, 40}}, :p2), do: {:win, :p2}
-
-  defmodule Deuce do
-    @type state() :: :"40-40" | :"ad-40" | :"40-ad"
-    @type player() :: :p1 | :p2
-
-    @spec new() :: :"40-40"
-    def new, do: :"40-40"
-
-    @spec next_state(state(), player()) :: {:cont, state()} | {:win, player()}
-
-    def next_state(:"40-40", :p1), do: {:cont, :"ad-40"}
-    def next_state(:"40-40", :p2), do: {:cont, :"40-ad"}
-    def next_state(:"ad-40", :p1), do: {:win, :p1}
-    def next_state(:"ad-40", :p2), do: {:cont, :"40-40"}
-    def next_state(:"40-ad", :p1), do: {:cont, :"40-40"}
-    def next_state(:"40-ad", :p2), do: {:win, :p2}
-  end
 end
--- test/tennis/game_test.exs
+++ test/tennis/game_test.exs
@@ -4,15 +4,59 @@ defmodule Tennis.GameTest do
   alias Tennis.Game
   alias Tennis.Game.Deuce
 
+  describe "Deuce" do
+    test "new/0 initializes correctly" do
+      assert Deuce.new() == :"40-40"
+    end
+
+    test "next_state/2 transitions between continue states" do
+      assert Deuce.next_state(:"40-40", :p1) == :"ad-40"
+      assert Deuce.next_state(:"ad-40", :p2) == :"40-40"
+      assert Deuce.next_state(:"40-ad", :p1) == :"40-40"
+    end
+
+    test "next_state/2 transitions to win states" do
+      assert Deuce.next_state(:"ad-40", :p1) == {:win, :p1}
+      assert Deuce.next_state(:"40-ad", :p2) == {:win, :p2}
+    end
+  end
+
   describe "Game" do
     test "new/0 initializes correctly" do
       assert Game.new() == {:regular, {0, 0}}
     end
-  end
 
-  describe "Deuce" do
-    test "new/0 initializes correctly" do
-      assert Deuce.new() == :"40-40"
+    test "next_state/2 increments scores correctly" do
+      Enum.each([0, 15, 30], fn score ->
+        assert Game.next_state({:regular, {0, score}}, :p1) == {:regular, {15, score}}
+        assert Game.next_state({:regular, {15, score}}, :p1) == {:regular, {30, score}}
+        assert Game.next_state({:regular, {30, score}}, :p1) == {:regular, {40, score}}
+
+        assert Game.next_state({:regular, {score, 0}}, :p2) == {:regular, {score, 15}}
+        assert Game.next_state({:regular, {score, 15}}, :p2) == {:regular, {score, 30}}
+        assert Game.next_state({:regular, {score, 30}}, :p2) == {:regular, {score, 40}}
+      end)
+    end
+
+    test "next_state/2 enters deuce states correctly" do
+      assert Game.next_state({:regular, {30, 40}}, :p1) == {:deuce, Deuce.new()}
+      assert Game.next_state({:regular, {40, 30}}, :p2) == {:deuce, Deuce.new()}
+    end
+
+    test "next_state/2 transitions through deuce states correctly" do
+      return_to_deuce =
+        {:deuce, Deuce.new()}
+        |> Game.next_state(:p1)
+        |> Game.next_state(:p2)
+
+      assert return_to_deuce == {:deuce, Deuce.new()}
+
+      p1_win =
+        return_to_deuce
+        |> Game.next_state(:p1)
+        |> Game.next_state(:p1)
+
+      assert p1_win == {:win, :p1}
     end
   end
 end
