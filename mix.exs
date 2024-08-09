3afe89c40b95d43837e5124c869a2689c57d44e7
Graham Preston <graham.preston@appfolio.com>
2024-08-09 09:26:13 -0700

    Move git repo root (whoops)

--- mix.exs
+++ mix.exs
@@ -1,33 +1,21 @@
-defmodule ScoreBuilder.MixProject do
+defmodule Olympic.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :score_builder,
+      apps_path: "apps",
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
-      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
-    ]
+    []
   end
 end
